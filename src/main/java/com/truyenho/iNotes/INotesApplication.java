2fe5d5b1a4da7f753702da9e6efbe97a12474e40
truyenho95 <truyenho.webdev@gmail.com>
2019-01-21 14:25:48 +0700

    first run

---
+++ src/main/java/com/truyenho/iNotes/INotesApplication.java
@@ -0,0 +1,14 @@
+package com.truyenho.iNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class INotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(INotesApplication.class, args);
+	}
+
+}
+
